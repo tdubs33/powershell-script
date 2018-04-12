edba1162b8bcb71f0736000b1c3ecbd51278dd71
tdubs33 <38074880+tdubs33@users.noreply.github.com>
2018-04-12 15:15:59 -0400

    Add files via upload

---
+++ vmss_iis_config.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module "WebAdministration"
+#Set up directories for web content
+New-Item c:\web\company.com\webservice1 -type directory
+New-Item c:\web\company.com\webservice2 -type directory
+New-Item c:\web\company.com\webservice3 -type directory
+#Add a page so we know IIS is working
+Set-Content c:\web\company.com\webservice1\default.htm "Welcome to webservice1"
+Set-Content c:\web\company.com\webservice2\default.htm "Welcome to webservice2"
+Set-Content c:\web\company.com\webservice3\default.htm "Welcome to webservice3"
+#make some webservices
+New-Item 'IIS:\Sites\Default Web Site\webservice1' -Type Application -PhysicalPath c:\web\company.com\webservice1
+Set-ItemProperty 'IIS:\Sites\Default Web Site\webservice1' -name applicationPool -value '.NET v4.5 Classic'
+New-Item 'IIS:\Sites\Default Web Site\webservice2' -Type Application -PhysicalPath c:\web\company.com\webservice2
+Set-ItemProperty 'IIS:\Sites\Default Web Site\webservice2' -name applicationPool -value '.NET v4.5 Classic'
+New-Item 'IIS:\Sites\Default Web Site\webservice3' -Type Application -PhysicalPath c:\web\company.com\webservice3
+Set-ItemProperty 'IIS:\Sites\Default Web Site\webservice3' -name applicationPool -value '.NET v4.5 Classic'
